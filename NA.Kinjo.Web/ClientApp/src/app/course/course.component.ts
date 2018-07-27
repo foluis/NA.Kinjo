e4ab54b496c2257c46e573bf9068b4bd20bcd200
Luis Fernando <foluis@hotmail.com>
2018-07-26 23:21:48 -0500

    Add course component (example)

---
+++ NA.Kinjo.Web/ClientApp/src/app/course/course.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-course',
+    templateUrl: './course.component.html',
+    styleUrls: ['./course.component.css']
+})
+/** course component*/
+export class CourseComponent {
+
+  title = "Courses";
+    constructor() {
+
+    }
+}
